42f42d4646c7083e12fa4094b38aec6d757b9311
spalger <spencer@elastic.co>
2022-06-23 09:35:47 -0500

    remove setup scripts that I don't use anymore

--- setup/git.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-username="$(whoami)"
-
-git config --global user.name "${username}"
-git config --global user.email "${username}@users.noreply.github.com"
-git config --global credential.helper osxkeychain
